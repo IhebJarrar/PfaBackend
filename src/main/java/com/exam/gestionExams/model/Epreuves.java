10c2441d12065171b1fa289f07fadec62fecec3b
medbk <mohameddbarka@gmail.com>
2018-05-26 20:15:13 +0100

    ajout style a l'imprime

--- src/main/java/com/exam/gestionExams/model/Epreuves.java
+++ src/main/java/com/exam/gestionExams/model/Epreuves.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -30,7 +31,7 @@ public class Epreuves {
 	@JoinColumn(name="groupe")
 	private Groupe groupe;
 	@JsonIgnore
-	@ManyToOne()
+	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="matiere")
 	private Matiere matiere;
 
--- src/main/java/com/exam/gestionExams/services/EpreuveServiceImpl.java
+++ src/main/java/com/exam/gestionExams/services/EpreuveServiceImpl.java
@@ -1,6 +1,7 @@
 package com.exam.gestionExams.services;
 
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import com.exam.gestionExams.model.Surveillant;
@@ -33,7 +34,7 @@ public class EpreuveServiceImpl implements EpreuveService {
 
 	@Override
 	public void epreuveToPdf(Epreuves e) {
-		String result = "/home/iheb/Bureau/pdf/epreuve"+e.getId()+".pdf";
+		String result = "C:/Users/mohamed/Desktop/epreuve"+e.getId()+".pdf";///home/iheb/Bureau/pdf/epreuve
 		try {
 
 			// step 1
@@ -53,11 +54,19 @@ public class EpreuveServiceImpl implements EpreuveService {
 			document.add(p2);
 
 			// salle rectangle
+	        Font fontLabel = new Font(FontFamily.HELVETICA, 16, Font.BOLD);
+	        Font fontData = new Font(FontFamily.TIMES_ROMAN, 18, Font.ITALIC, BaseColor.GRAY);
 			PdfPTable table = new PdfPTable(1);
-			String nomSalle="";
-			if(e.getLocal()!=null) nomSalle=e.getLocal().getNom()+"E"+e.getLocal().getEtage();
-			Paragraph salle= new Paragraph("Salle : "+nomSalle+"\n", new Font(FontFamily.TIMES_ROMAN, 22));
-			PdfPCell cell = new PdfPCell(new Phrase(salle));
+			Chunk chunkLabel = new Chunk("Salle :",fontLabel);
+			Paragraph paragrapheSalle= new Paragraph();
+			paragrapheSalle.add(chunkLabel);
+			Chunk chunkData = new Chunk("",fontData);
+			
+			if(e.getLocal()!=null) {
+				chunkData.append(e.getLocal().getNom()+"E"+e.getLocal().getEtage());
+			}
+			paragrapheSalle.add(chunkData);
+			PdfPCell cell = new PdfPCell(paragrapheSalle);
 			cell.setRowspan(400);
 			table.addCell(cell);
 			table.setWidthPercentage(20);
@@ -77,12 +86,45 @@ public class EpreuveServiceImpl implements EpreuveService {
 			cellGauche.setBorder(PdfPCell.NO_BORDER);
 			//le tableau du gauche
 			PdfPTable rectDonnée = new PdfPTable(1);
-			Paragraph donnée= new Paragraph("Date : "+e.getCreneau().getDate()+"\nSeance : "+e.getCreneau().getSeance().getHeureDebut()+"\nDurée(h) : "+e.getDuree()+"\nParcours : "+e.getGroupe().getClasse().getNivSpecialite().getSpecialite().getNom()
-					+ "\nNiveau : "+e.getGroupe().getClasse().getNivSpecialite().getNiveau().getNiveau()+"\nN° EPV : "+e.getId()+"\nNom Epreuve : "+e.getNom()+"\n ", new Font(FontFamily.TIMES_ROMAN, 15,Font.BOLD));
-			PdfPCell cellrectDonnée = new PdfPCell(new Phrase(donnée));
+			Paragraph paragrapheDataTableLeft= new Paragraph();
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		    String dateFormatted = formatter.format(e.getCreneau().getDate());
+		    Chunk chunkLabelDate = new Chunk("Date :",fontLabel);
+		    paragrapheDataTableLeft.add(chunkLabelDate);
+			Chunk chunkDate = new Chunk(dateFormatted,fontData);
+			paragrapheDataTableLeft.add(chunkDate);
+			Chunk chunkLabelSeance = new Chunk("\nSeance : ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelSeance);
+			Chunk chunkSeance = new Chunk(e.getCreneau().getSeance().getHeureDebut(),fontData);
+			paragrapheDataTableLeft.add(chunkSeance);
+			Chunk chunkLabelDuree = new Chunk("Durée(h) :  ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelDuree);
+			Chunk chunkDuree = new Chunk(e.getDuree().toString(),fontData);
+			paragrapheDataTableLeft.add(chunkDuree);
+			Chunk chunkLabelParcours = new Chunk("\nParcours :  ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelParcours);
+			Chunk chunkParcours = new Chunk(e.getGroupe().getClasse().getNivSpecialite().getSpecialite().getNom(),fontData);
+			paragrapheDataTableLeft.add(chunkParcours);
+			Chunk chunkLabelNiveau = new Chunk("\nNiveau : ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelNiveau);
+			Chunk chunkNiveau = new Chunk(String.valueOf(e.getGroupe().getClasse().getNivSpecialite().getNiveau().getNiveau()),fontData);
+			paragrapheDataTableLeft.add(chunkNiveau);
+			Chunk chunkLabelNumEpr = new Chunk("\nN° EPV : ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelNumEpr);
+			Chunk chunkNumEpr = new Chunk(String.valueOf(e.getId()),fontData);
+			paragrapheDataTableLeft.add(chunkNumEpr);
+			Chunk chunkLabelNomEpr = new Chunk("\nNom Epreuve : ",fontLabel);
+			paragrapheDataTableLeft.add(chunkLabelNomEpr);
+			Chunk chunkNom = new Chunk(e.getNom()+"\n",fontData);
+			paragrapheDataTableLeft.add(chunkNom);
+			PdfPCell cellrectDonnée = new PdfPCell(paragrapheDataTableLeft);
 			rectDonnée.addCell(cellrectDonnée);
 			//le reste du gauche
-			Paragraph groupe=new Paragraph("Groupe : "+e.getGroupe().getNom()+"\n",new Font(FontFamily.TIMES_ROMAN, 15,Font.BOLD));
+			Paragraph groupe=new Paragraph();
+			Chunk chunkLabelGroupe = new Chunk("\nGroupe : ",fontLabel);
+			Chunk chunkGroupe = new Chunk(e.getGroupe().getNom()+"\n",fontData);
+			groupe.add(chunkLabelGroupe);
+			groupe.add(chunkGroupe);
 			Paragraph details=new Paragraph("Détails de l'épreuve : \n\n\n\n\n\n",new Font(FontFamily.TIMES_ROMAN, 15,Font.ITALIC));
 			//finaliser la gauche
 			cellGauche.addElement(rectDonnée);
@@ -103,28 +145,24 @@ public class EpreuveServiceImpl implements EpreuveService {
 			//cas des surveillants
 			List<Surveillant>surv=e.getSurveillants();
 			String survResponsable="";
-			StringBuilder survRestant=new StringBuilder("\n");
+			StringBuilder surveillants=new StringBuilder("\n");
 			if(surv!=null)
 			{
 				switch (surv.size()) {
 					case 1:
 					{
-						survResponsable=surv.get(0).getNom();
-						survRestant.append("\n\n");
+						surveillants.append("   "+surv.get(0).getNom()+"\n\n");
 						break;
 					}
 					case 2:
 					{
-						survResponsable=surv.get(0).getNom();
-						survRestant.append(surv.get(1).getNom()+"\n");
+						surveillants.append("   "+surv.get(0).getNom()+"\n   "+surv.get(1).getNom()+"\n");
 						break;
 					}
 
 					case 3:
 					{
-						survResponsable=surv.get(0).getNom();
-						survRestant.append(surv.get(1).getNom()+"\n");
-						survRestant.append(surv.get(2).getNom()+"\n");
+						surveillants.append("   "+surv.get(0).getNom()+"\n   "+surv.get(1).getNom()+"\n   "+surv.get(2).getNom()+"\n");
 						break;
 					}
 					default:
@@ -133,7 +171,7 @@ public class EpreuveServiceImpl implements EpreuveService {
 				}
 			}
 
-			Paragraph dataDroite2 =new Paragraph("Enseignant(s) Responsable(s) : "+survResponsable+"\n Surveillant(s) : "+survRestant,new Font(FontFamily.TIMES_ROMAN, 15,Font.BOLD));
+			Paragraph dataDroite2 =new Paragraph("Enseignant(s) Responsable(s) : "+survResponsable+"\nSurveillant(s) : "+surveillants,new Font(FontFamily.TIMES_ROMAN, 15,Font.BOLD));
 
 			//tableau absent
 			PdfPTable tableAbsent = new PdfPTable(1);
